6bf0cf56bb426f025901e3642c6cd09befb9002c
Dale Fukami <dale.fukami@gmail.com>
2015-01-28 07:32:12 -0700

    Some initial aliases

---
+++ accurev.sh
@@ -0,0 +1,9 @@
+
+alias acc='accurev'
+alias ast='accurev stat --outgoing'
+alias aadd='accurev add -R .'
+alias arm='accurev defunct'
+alias acommit='accurev promote -k'
+
+#accurev keep -c "Move timesheet config to a separate model" -m
+#accurev merge ./app/controllers/user/timesheet-settings/edit.js
